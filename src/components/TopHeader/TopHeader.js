5dd50b15c0e7010b65f57aa2eba38dcbfba59760
BayajidAlam <bayajidalam2001@gmail.com>
2023-02-24 07:47:33 +0600

    top header added

---
+++ src/components/TopHeader/TopHeader.js
@@ -0,0 +1,58 @@
+import React from "react";
+import "./TopHeader.css";
+import { FaFacebookF } from 'react-icons/fa';
+import { BsInstagram,BsWhatsapp,BsTwitter,BsPinterest } from 'react-icons/bs';
+
+const TopHeader = () => {
+  return (
+    <section className="bg-[#2D373C] text-white">
+      <div className="flex container mx-auto justify-between items-center">
+        <div className="flex items-center">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-6 h-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 002.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 01-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 00-1.091-.852H4.5A2.25 2.25 0 002.25 4.5v2.25z"
+            />
+          </svg>
+          <p className="ml-1"> +1 763-227-5032</p>
+        </div>
+
+        <div className="flex items-center">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-6 h-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M21.75 9v.906a2.25 2.25 0 01-1.183 1.981l-6.478 3.488M2.25 9v.906a2.25 2.25 0 001.183 1.981l6.478 3.488m8.839 2.51l-4.66-2.51m0 0l-1.023-.55a2.25 2.25 0 00-2.134 0l-1.022.55m0 0l-4.661 2.51m16.5 1.615a2.25 2.25 0 01-2.25 2.25h-15a2.25 2.25 0 01-2.25-2.25V8.844a2.25 2.25 0 011.183-1.98l7.5-4.04a2.25 2.25 0 012.134 0l7.5 4.04a2.25 2.25 0 011.183 1.98V19.5z"
+            />
+          </svg>
+          <p className="ml-1">tourxpro@gmail.com</p>
+        </div>
+        <p className="capitalize bg-[#FF4838] py-3 px-12 font-bold">Black Friday Big Offer</p>
+        <div className="flex items-center space-x-3 text-[#FF4838]">
+            <FaFacebookF/>
+            <BsInstagram/>
+            <BsTwitter/>
+            <BsWhatsapp/>
+            <BsPinterest/>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default TopHeader;
